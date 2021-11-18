93affc688b9b78a9cb7e758915d197c4b18e145c
u914013 <nicolas.gonzalez@baufest.com>
2021-11-18 14:18:09 -0300

    up version

--- src/connect/index.js
+++ src/connect/index.js
@@ -1,11 +1,14 @@
 const utils = require('./utils')
+
 async function test() {
   console.log(utils)
-  utils.sendMessage({ cola: 'payments', message: Buffer.from(JSON.stringify({name: "nuevo elemento", lastName: "Plasmas" }))})
-  /* sendMessage({ cola: 'payments', message: Buffer.from(JSON.stringify({name: "pedro", lastName: "alfonso"})) })
+  utils.sendMessage({ cola: 'payments', message: Buffer.from(JSON.stringify({name: "backo", lastName: "backo_web" }))})
+  /* sendMessage({ channel: 'payments', message: Buffer.from(JSON.stringify({name: "pedro", lastName: "alfonso"})) })
   sendMessage({ cola: 'payments', message: Buffer.from(JSON.stringify({name: "ricardo", lastName: "palacios" }))})
   sendMessage({ cola: 'payments', message: Buffer.from(JSON.stringify({ name: "david", lastName: "pepinos" })) })
   sendMessage({ cola: 'payments', message: Buffer.from(JSON.stringify({ name: "david", lastName: "pepinos" })) }) */
+  utils.getMessage({ cola: 'payments' })
+  utils.getMessage({ cola: 'payments' })
   utils.getMessage({ cola: 'payments' })
 }
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,23 @@
 const express = require('express')
-
+const utils = require('../connect/utils')
 const router = express.Router();
 
-router.get('/hi', async (req, res) => {
+router.get('/message/:name/:lastName', async (req, res) => {
   console.log('hola')
+  const {name, lastName} = req.params
+  utils.sendMessage({ cola: 'payments', message: Buffer.from(JSON.stringify({name: name, lastName: lastName }))})
   res.sendStatus(200)
 })
 
 module.exports = router;
+
+/*
+  tamaño
+  message package
+  binary message
+  nomenclatura
+  envio informacion completa
+  configuracion del componente
+  factory, funcion a algo, le mando un permiso o otro
+  SOLID
+*/
